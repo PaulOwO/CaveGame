5717a1e730be5b097df19488883a335c9b489be7
chevre hurlante lol <paul.breuza@gmail.com>
2021-05-07 15:54:04 +0200

    Shooting and reloading

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D _body;
+    [SerializeField] private Camera _camera;
+
+    private Vector2 _mousePosition;
+    private Vector2 _lookDirection;
+
+    private float _angle;
+
+    
+    void Update()
+    {
+       _mousePosition = _camera.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    private void FixedUpdate()
+    {
+        _lookDirection = _mousePosition - _body.position;
+        _angle = Mathf.Atan2(_lookDirection.y, _lookDirection.x) * Mathf.Rad2Deg - 90f;
+        _body.rotation = _angle;
+    }
+}
---
+++ Assets/Scripts/Shooting.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    [SerializeField] private Transform _firePoint;
+    [SerializeField] private GameObject _bulletPrefab;
+    [SerializeField] private float _bulletSpeed = 20f;
+
+    private GameObject _bulletInstance;
+    private Rigidbody2D _bodyInstance;
+    private float _bulletCharged = 6f;
+    private bool _reloading = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_bulletCharged > 0)
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                Shoot();
+            }
+        }
+        else if (_reloading == false)
+        {
+            StartCoroutine(Reload());
+        }
+    }
+
+    private void Shoot()
+    {
+        _bulletCharged = _bulletCharged - 1f;
+        _bulletInstance = Instantiate(_bulletPrefab, _firePoint.position, _firePoint.rotation);
+        _bodyInstance = _bulletInstance.GetComponent<Rigidbody2D>();
+        _bodyInstance.AddForce(_firePoint.up * _bulletSpeed, ForceMode2D.Impulse);
+    }
+    
+    IEnumerator Reload()
+    {
+        _reloading = true;
+        Debug.Log("Reloading");
+        yield return new WaitForSeconds(2f);
+        _bulletCharged = 6f;
+        _reloading = false;
+    }
+}
