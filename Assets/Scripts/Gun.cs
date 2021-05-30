2641e85b6ccc68e6fa4cf8850a26ac9322c5985c
Aidan Cuennet <aidan.cuennet@gmail.com>
2021-05-30 21:35:22 +0200

    commit

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D _body;
+    [SerializeField] private Camera _camera;
+    [SerializeField] private Transform _cursor;
+    private Vector2 _mousePosition;
+    private Vector2 _lookDirection;
+
+    private float _cameraOffSet = -10;
+    private float _angle;
+    private Vector2 movement;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        _mousePosition = _camera.ScreenToWorldPoint(Input.mousePosition);
+        _cursor.position = _mousePosition;
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        _camera.transform.position = this.transform.position + new Vector3(0, 0, _cameraOffSet);
+    }
+
+    private void FixedUpdate()
+    {
+        _lookDirection = _mousePosition - _body.position;
+        _angle = Mathf.Atan2(_lookDirection.y, _lookDirection.x) * Mathf.Rad2Deg - 90f;
+        _body.rotation = _angle;
+        
+    }
+
+}
