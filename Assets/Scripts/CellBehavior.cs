af48a6bfda6ba04f89aad238d5f08280d6373174
chevre hurlante lol <paul.breuza@gmail.com>
2021-05-31 11:56:08 +0200

    started cellular automata and changed bullet

---
+++ Assets/Scripts/CellBehavior.cs
@@ -0,0 +1,32 @@
+﻿
+using UnityEngine;
+public struct Cell
+{
+    public bool isAlive;
+
+    public Cell(bool isAlive)
+    {
+        this.isAlive = isAlive;
+    }
+}
+public class CellBehavior : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer spriteRenderer;
+    private bool isAlive = true;
+
+
+    public bool IsAlive
+    {
+        get => isAlive;
+        set
+        {
+            isAlive = value;
+            UpdateColor(isAlive?Color.white:Color.black);
+        }
+    }
+
+    public void UpdateColor(Color color)
+    {
+        spriteRenderer.color = color;
+    }
+}
