90ddd836fbc7aa1640881c226dca81f11cb063aa
chevre hurlante lol <paul.breuza@gmail.com>
2021-06-03 13:31:39 +0200

    UI, audio and shooting upgraded

---
+++ Assets/Scripts/CellularAutomata/CellBehavior.cs
@@ -0,0 +1,37 @@
+﻿
+using UnityEngine;
+public struct Cell
+{
+    public bool isAlive;
+
+    public Cell(bool isAlive)
+    {
+        this.isAlive = isAlive;
+    }
+}
+public class CellBehavior : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer spriteRenderer;
+    [SerializeField] private Sprite[] _sprites;
+    private bool isAlive = true;
+
+
+    private void Start()
+    {
+       // spriteRenderer.sprite = _sprites[Random.Range(0, _sprites.Length)];
+    }
+    public bool IsAlive
+    {
+        get => isAlive;
+        set
+        {
+            isAlive = value;
+            UpdateColor(isAlive?Color.white:Color.black);
+        }
+    }
+
+    public void UpdateColor(Color color)
+    {
+        spriteRenderer.color = color;
+    }
+}
